f6a00d66d891e397a215d79283aa65db9984ea19
yevhen <garret.evg@gmail.com>
2026-09-12 15:38:08 +0200

    bump version to 1.0.4

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.evgeniich.flashread"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     packaging {
